5182b6809e7082cb51bf2197d3b9f91a45938987
Gordey Doronin <g.doronin@gmail.com>
2013-08-21 03:29:10 +0700

    - initial project;

---
+++ js/data.js
@@ -0,0 +1,22 @@
+(function () {
+	var data = window.data = {
+		seasons: {
+			1: {
+				period: null,
+				episodes: {
+					1: {
+						code: '1x01',
+						title: {
+							eng: 'Ambush',
+							rus: 'Засада'
+						},
+						intro: {
+							eng: 'Great leaders inspire greatness in others.',
+							rus: 'Великие лидеры вдохновляют других на великие свершения.'
+						}
+					}
+				}
+			}
+		}
+	};
+})();
