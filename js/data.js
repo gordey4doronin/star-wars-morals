0d2e23c3a447d622b1bce7c06515cd78183a3ddd
Gordey Doronin <g.doronin@gmail.com>
2013-08-22 02:32:35 +0700

    - filling first season WIP;

--- js/data.js
+++ js/data.js
@@ -5,7 +5,8 @@
 				period: null,
 				episodes: {
 					1: {
-						code: '1x01',
+						id: 1,
+						productionId: '1.08',
 						title: {
 							eng: 'Ambush',
 							rus: 'Засада'
@@ -13,6 +14,258 @@
 						intro: {
 							eng: 'Great leaders inspire greatness in others.',
 							rus: 'Великие лидеры вдохновляют других на великие свершения.'
+						}
+					}
+					2: {
+						id: 2,
+						productionId: '1.07',
+						title: {
+							eng: 'Rising Malevolence',
+							rus: 'Наступление «Зловещего»'
+						},
+						intro: {
+							eng: 'Belief is not a matter of choice, but of conviction.',
+							rus: 'Веру выбирают, а не навязывают.'
+						}
+					}
+					3: {
+						id: 3,
+						productionId: '1.09',
+						title: {
+							eng: 'Shadow of Malevolence',
+							rus: 'Тень «Зловещего»'
+						},
+						intro: {
+							eng: 'Easy is the path to wisdom for those not blinded by themselves.',
+							rus: 'Легок путь к мудрости для тех, кто не ослеплен собой.'
+						}
+					}
+					4: {
+						id: 4,
+						productionId: '1.11',
+						title: {
+							eng: 'Destroy Malevolence',
+							rus: 'Гибель «Зловещего»'
+						},
+						intro: {
+							eng: 'A plan is only as good as those who see it through.',
+							rus: 'Удачен тот план, что продуман до конца.'
+						}
+					}
+					5: {
+						id: 5,
+						productionId: '1.14',
+						title: {
+							eng: 'Rookies',
+							rus: 'Салаги'
+						},
+						intro: {
+							eng: 'The best confidence builder is experience.',
+							rus: 'Уверенность приходит с опытом.'
+						}
+					}
+					6: {
+						id: 6,
+						productionId: '1.02',
+						title: {
+							eng: 'Downfall of a Droid',
+							rus: 'Утрата дройда'
+						},
+						intro: {
+							eng: 'Trust in your friends, and they''ll have reason to trust in you.',
+							rus: 'Доверься другу и тогда друг доверится тебе.'
+						}
+					}
+					7: {
+						id: 7,
+						productionId: '1.06',
+						title: {
+							eng: 'Duel of the Droids',
+							rus: 'Дуэль дроидов'
+						},
+						intro: {
+							eng: 'You hold onto friends by keeping your heart a little softer than your head.',
+							rus: 'Держись друзей своих, чтобы сердце твое не стало холодным как разум.'
+						}
+					}
+					8: {
+						id: 8,
+						productionId: '1.05',
+						title: {
+							eng: 'Bombad Jedi',
+							rus: 'Джедая бомбад'
+						},
+						intro: {
+							eng: 'Heroes are made by the times.',
+							rus: 'Героев создает время.'
+						}
+					}
+					9: {
+						id: 9,
+						productionId: '1.10',
+						title: {
+							eng: 'Cloak of Darkness',
+							rus: 'Маска Тьмы'
+						},
+						intro: {
+							eng: 'Ignore your instincts at your peril.',
+							rus: 'Не бойся идти наперекор своим инстинктам.'
+						}
+					}
+					10: {
+						id: 10,
+						productionId: '1.12',
+						title: {
+							eng: 'Lair of Grievous',
+							rus: 'Логово Гривуса'
+						},
+						intro: {
+							eng: 'Most powerful is he who controls his own power.',
+							rus: 'Могущества достигает лишь тот, кто контролирует свою силу.'
+						}
+					}
+					11: {
+						id: 11,
+						productionId: '1.16',
+						title: {
+							eng: 'Dooku Captured',
+							rus: 'Дуку в плену'
+						},
+						intro: {
+							eng: 'The winding path to peace is always a worthy one, regardless of how many turns it takes.',
+							rus: 'Извилист путь к миру, но бесценен. Сколько бы препятствий к нему не было.'
+						}
+					}
+					12: {
+						id: 12,
+						productionId: '1.20',
+						title: {
+							eng: 'The Gungan General',
+							rus: 'Генерал Гунган'
+						},
+						intro: {
+							eng: 'Fail with honor rather than succeed by fraud.',
+							rus: 'Лучше проиграть с честью, чем преуспеть с помощью обмана.'
+						}
+					}
+					13: {
+						id: 13,
+						productionId: '1.22',
+						title: {
+							eng: 'Jedi Crash',
+							rus: 'Авария джедаев'
+						},
+						intro: {
+							eng: 'Greed and fear of loss are the roots that lead to the tree of evil.',
+							rus: 'Ничто так не питает зло как жадность и страх поражения.'
+						}
+					}
+					14: {
+						id: 14,
+						productionId: '1.24',
+						title: {
+							eng: 'Defenders of Peace',
+							rus: 'Хранители мира'
+						},
+						intro: {
+							eng: 'When surrounded by war, one must eventually choose a side.',
+							rus: 'Когда война пришла в твой дом, сохранять нейтралитет уже невозможно.'
+						}
+					}
+					15: {
+						id: 15,
+						productionId: '1.25',
+						title: {
+							eng: 'Trespass',
+							rus: 'Нарушители границы'
+						},
+						intro: {
+							eng: 'Arrogance diminishes wisdom.',
+							rus: 'Гордыня и мудрого к гибели приведет.'
+						}
+					}
+					16: {
+						id: 16,
+						productionId: '2.01',
+						title: {
+							eng: 'The Hidden Enemy',
+							rus: 'Скрытый враг'
+						},
+						intro: {
+							eng: '',
+							rus: 'Правда открывает глаза, но сердцу часто причиняет боль.'
+						}
+					}
+					17: {
+						id: 17,
+						productionId: '1.26',
+						title: {
+							eng: 'Blue Shadow Virus',
+							rus: 'Вирус «Синий призрак»'
+						},
+						intro: {
+							eng: '',
+							rus: 'Страх — это болезнь, единственное лекарство от него — надежда.'
+						}
+					}
+					18: {
+						id: 18,
+						productionId: '2.02',
+						title: {
+							eng: 'Mystery of a Thousand Moons',
+							rus: 'Тайна тысячи лун'
+						},
+						intro: {
+							eng: '',
+							rus: 'Случайность может спасти галактику.'
+						}
+					}
+					19: {
+						id: 19,
+						productionId: '1.15',
+						title: {
+							eng: 'Storm Over Ryloth',
+							rus: 'Сражение при Рилоте'
+						},
+						intro: {
+							eng: '',
+							rus: 'Тяжек путь к величию.'
+						}
+					}
+					20: {
+						id: 20,
+						productionId: '1.17',
+						title: {
+							eng: 'Innocents of Ryloth',
+							rus: 'Невинные жертвы Рилота'
+						},
+						intro: {
+							eng: '',
+							rus: 'Невозможно исчислить цену войны.'
+						}
+					}
+					21: {
+						id: 21,
+						productionId: '1.19',
+						title: {
+							eng: 'Liberty on Ryloth',
+							rus: 'Освобождение Рилота'
+						},
+						intro: {
+							eng: '',
+							rus: 'Компромисс — добродетель которую нужно возделывать, а не слабость, которую нужно презирать.'
+						}
+					}
+					22: {
+						id: 22,
+						productionId: '2.04',
+						title: {
+							eng: 'Hostage Crisis',
+							rus: 'Захват заложников'
+						},
+						intro: {
+							eng: '',
+							rus: 'Общая тайна формирует доверие.'
 						}
 					}
 				}
