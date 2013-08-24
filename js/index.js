a1eaddb2e66b9da593a89f230d7ed8fd178c6012
Gordey Doronin <g.doronin@gmail.com>
2013-08-25 00:27:59 +0700

    Added main title sound. Works for IE 9+, Chrome 6+, Safari 4+. TODO: add *.ogg or *.wav for Opera and Firefox.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
+    // create and bind viewmodel
     var viewModel = new Application.ViewModel(Application.data);
     viewModel.selectRandomEpisode();
     ko.applyBindings(viewModel);
 
+    // set language toggling
     setInterval(function () {
         viewModel.toggleLang();
     }, 5000);
+
+    // set sound volume
+    $('audio').get(0).volume = 0.3;
 });
