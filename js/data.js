c9cbb57129c95a630051af1ea4b95bfdd07b61e6
Gordey Doronin <g.doronin@gmail.com>
2013-09-19 00:17:20 +0700

    Filling second season data done.

--- js/data.js
+++ js/data.js
@@ -4,7 +4,8 @@
     data.seasons = [
         {
             id: 1,
-            period: null,
+            premiere: '2008/10/03',
+            finale: '2009/03/20',
             episodes: [
                 {
                     id: 1,
@@ -274,8 +275,274 @@
         },
         {
             id: 2,
-            period: null,
-            episodes: []
+            premiere: '2009/10/02',
+            finale: '2010/04/30',
+            episodes: [
+                {
+                    id: 23,
+                    productionId: '1.23',
+                    title: {
+                        eng: 'Holocron Heist',
+                        rus: 'Похищение Голокрона'
+                    },
+                    moral: {
+                        eng: 'A lesson learned is a lesson earned.',
+                        rus: 'На пользу идет лишь заслуженный урок.'
+                    }
+                },
+                {
+                    id: 24,
+                    productionId: '1.13',
+                    title: {
+                        eng: 'Cargo of Doom',
+                        rus: 'Корабль обречённых'
+                    },
+                    moral: {
+                        eng: 'Overconfidence is the most dangerous form of carelessness.',
+                        rus: 'Чрезмерная уверенность — самая опасная форма беспечности.'
+                    }
+                },
+                {
+                    id: 25,
+                    productionId: '2.03',
+                    title: {
+                        eng: 'Children of the Force',
+                        rus: 'Дети Силы'
+                    },
+                    moral: {
+                        eng: 'The first step to correcting a mistake is patience.',
+                        rus: 'Первый шаг на пути исправления — терпение.'
+                    }
+                },
+                {
+                    id: 26,
+                    productionId: '2.05',
+                    title: {
+                        eng: 'Senate Spy',
+                        rus: 'Шпион Сената'
+                    },
+                    moral: {
+                        eng: 'A true heart should never be doubted.',
+                        rus: 'Любящее сердце не знает сомнений.'
+                    }
+                },
+                {
+                    id: 27,
+                    productionId: '2.07',
+                    title: {
+                        eng: 'Landing at Point Rain',
+                        rus: 'Высадка в Пойнт Рэйн'
+                    },
+                    moral: {
+                        eng: 'Believe in yourself or no one else will.',
+                        rus: 'Поверь в себя и в тебя поверят другие.'
+                    }
+                },
+                {
+                    id: 28,
+                    productionId: '2.08',
+                    title: {
+                        eng: 'Weapons Factory',
+                        rus: 'Оружейная фабрика'
+                    },
+                    moral: {
+                        eng: 'No gift is more precious than trust.',
+                        rus: 'Самый лучший подарок — это доверие.'
+                    }
+                },
+                {
+                    id: 29,
+                    productionId: '2.09',
+                    title: {
+                        eng: 'Legacy of Terror',
+                        rus: 'Наследие террора'
+                    },
+                    moral: {
+                        eng: 'Sometimes, accepting help is harder than offering it.',
+                        rus: 'Иногда труднее принять помощь, чем предложить её.'
+                    }
+                },
+                {
+                    id: 30,
+                    productionId: '2.12',
+                    title: {
+                        eng: 'Brain Invaders',
+                        rus: 'Контроль Разума'
+                    },
+                    moral: {
+                        eng: 'Attachment is not compassion.',
+                        rus: 'Преданность не означает сочувствие.'
+                    }
+                },
+                {
+                    id: 31,
+                    productionId: '2.14',
+                    title: {
+                        eng: 'Grievous Intrigue',
+                        rus: 'Происки Гривуса'
+                    },
+                    moral: {
+                        eng: 'For everything you gain, you lose something else.',
+                        rus: 'В одном месте найдёшь, в другом потеряешь.'
+                    }
+                },
+                {
+                    id: 32,
+                    productionId: '2.06',
+                    title: {
+                        eng: 'The Deserter',
+                        rus: 'Дезертир'
+                    },
+                    moral: {
+                        eng: 'It is the quest for honor that makes one honorable.',
+                        rus: 'Мы удостаиваемся чести, только если ищем её.'
+                    }
+                },
+                {
+                    id: 33,
+                    productionId: '2.11',
+                    title: {
+                        eng: 'Lightsaber Lost',
+                        rus: 'Потерянный меч'
+                    },
+                    moral: {
+                        eng: 'Easy isn\'t always simple.',
+                        rus: 'Просто не значит легко.'
+                    }
+                },
+                {
+                    id: 34,
+                    productionId: '2.13',
+                    title: {
+                        eng: 'The Mandalore Plot',
+                        rus: 'Мандалорский заговор'
+                    },
+                    moral: {
+                        eng: 'If you ignore the past, you jeopardize your future.',
+                        rus: 'Отказываясь от прошлого, ты рискуешь будущим.'
+                    }
+                },
+                {
+                    id: 35,
+                    productionId: '1.21',
+                    title: {
+                        eng: 'Voyage of Temptation',
+                        rus: 'Вояж искушений'
+                    },
+                    moral: {
+                        eng: 'Fear not for the future, weep not for the past.',
+                        rus: 'Не страшись будущего, не оплакивай прошлое.'
+                    }
+                },
+                {
+                    id: 36,
+                    productionId: '2.16',
+                    title: {
+                        eng: 'Duchess of Mandalore',
+                        rus: 'Герцогиня Мандалора'
+                    },
+                    moral: {
+                        eng: 'In war, truth is the first casualty.',
+                        rus: 'На войне прежде всего страдает истина.'
+                    }
+                },
+                {
+                    id: 37,
+                    productionId: '2.10',
+                    title: {
+                        eng: 'Senate Murders',
+                        rus: 'Убийства в Сенате'
+                    },
+                    moral: {
+                        eng: 'Searching for the truth is easy. Accepting the truth is hard.',
+                        rus: 'Найти истину несложно. Смириться с ней намного труднее.'
+                    }
+                },
+                {
+                    id: 38,
+                    productionId: '2.17',
+                    title: {
+                        eng: 'Cats and Mice',
+                        rus: 'Кошки — мышки'
+                    },
+                    moral: {
+                        eng: 'A wise leader knows when to follow.',
+                        rus: 'Мудрый лидер знает, когда слушать других.'
+                    }
+                },
+                {
+                    id: 39,
+                    productionId: '2.19',
+                    title: {
+                        eng: 'Bounty Hunters',
+                        rus: 'Наёмники'
+                    },
+                    moral: {
+                        eng: 'Courage makes heroes, but trust builds friendship.',
+                        rus: 'Храбрость делает тебя героем, но только доверие порождает дружбу.'
+                    }
+                },
+                {
+                    id: 40,
+                    productionId: '2.22',
+                    title: {
+                        eng: 'The Zillo Beast',
+                        rus: 'Гад Зилло'
+                    },
+                    moral: {
+                        eng: 'Choose what is right, not what is easy.',
+                        rus: 'Выбери правильный путь, а не тот по которому легче идти.'
+                    }
+                },
+                {
+                    id: 41,
+                    productionId: '2.23',
+                    title: {
+                        eng: 'The Zillo Beast Strikes Back',
+                        rus: 'Зилло наносит ответный удар'
+                    },
+                    moral: {
+                        eng: 'The most dangerous beast is the beast within.',
+                        rus: 'Самый опасный зверь — это внутренний зверь.'
+                    }
+                },
+                {
+                    id: 42,
+                    productionId: '2.15',
+                    title: {
+                        eng: 'Death Trap',
+                        rus: 'Смертельная ловушка'
+                    },
+                    moral: {
+                        eng: 'Who my father was matters less than my memory of him.',
+                        rus: 'Мой отец имеет меньшее значение, чем мои воспоминания о нём.'
+                    }
+                },
+                {
+                    id: 43,
+                    productionId: '2.18',
+                    title: {
+                        eng: 'R2 Come Home',
+                        rus: 'Ар-Два, лети домой!'
+                    },
+                    moral: {
+                        eng: 'Adversity is a friendship\'s truest test.',
+                        rus: 'Друзья познаются в беде.'
+                    }
+                },
+                {
+                    id: 44,
+                    productionId: '2.20',
+                    title: {
+                        eng: 'Lethal Trackdown',
+                        rus: 'Смертельная погоня'
+                    },
+                    moral: {
+                        eng: 'Revenge is a confession of pain.',
+                        rus: 'Месть — признание боли.'
+                    }
+                }
+            ]
         },
         {
             id: 3,
