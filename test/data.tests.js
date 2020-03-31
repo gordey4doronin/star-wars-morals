eb28c30060d65727b97dbd88245da89b9aa258d0
Gordey Doronin <g.doronin@gmail.com>
2020-03-31 12:51:27 +0700

    Improve tests

--- test/data.tests.js
+++ test/data.tests.js
@@ -1,49 +1,55 @@
-//noinspection JSUnusedGlobalSymbols
-var should = require('chai').should();
+require('chai').should();
 var data = require('../js/data');
 
-describe('data', function() {
-    describe('seasons', function () {
-        it('should have all fields', function () {
-            data.seasons.forEach(function (x) {
-                x.should.have.all.keys([
-                    'id',
-                    'premiere',
-                    'finale',
-                    'episodes'
-                ]);
-            });
-        });
-
-        it('should have length 6', function () {
+describe('data', () => {
+    describe('seasons', () => {
+        it('should have length 6', () => {
             data.seasons.should.have.length(6);
         });
 
-        it('should have proper id fields', function () {
-            data.seasons.forEach(function(x, index) {
-                x.id.should.equal(index + 1);
-           });
-        });
-    });
+        context('check number of episodes', () => {
+            const numbers = [22, 22, 22, 22, 20, 13];
 
-    describe('season 6 episodes', function () {
-        var season6 = data.seasons[5];
+            data.seasons.forEach((season, index) => {
+                const func = index === 2 ? xit : it; // TODO: Add season 3 data.
 
-        it('should have all fields', function () {
-            season6.episodes.forEach(function (x) {
-                x.should.have.all.keys([
-                    'id',
-                    'productionId',
-                    'order',
-                    'title',
-                    'moral'
-                ]);
+                func(`season ${index + 1} should have ${numbers[index]} episodes`, () => {
+                    season.episodes.should.have.length(numbers[index]);
+                });
             });
         });
 
-        it('should have proper order fields', function () {
-            season6.episodes.forEach(function(x, index) {
-                x.order.should.equal(index + 1);
+        context('check all required fields', () => {
+            data.seasons.forEach((season, index) => {
+                it(`season ${index + 1} should have all required fields`, () => {
+                    season.should.have.all.keys(['id', 'premiere', 'finale', 'episodes']);
+                });
+            });
+        });
+
+        context('check id values', () => {
+            data.seasons.forEach((season, index) => {
+                it(`season ${index + 1} should have proper id fields`, () => {
+                    season.id.should.equal(index + 1);
+                });
+            });
+        });
+    });
+
+    describe('episodes', () => {
+        context('check episodes order values', () => {
+            data.seasons.forEach((season, index) => {
+                it(`season ${index + 1} episodes should have proper order fields`, () => {
+                    season.episodes.forEach((episode, episodeIndex) => episode.order.should.equal(episodeIndex + 1));
+                });
+            });
+        });
+
+        context('check episodes all required fields', () => {
+            data.seasons.forEach((season, index) => {
+                it(`season ${index + 1} episodes should have all required fields`, () => {
+                    season.episodes.forEach((episode) => episode.should.have.all.keys(['id', 'productionId', 'order', 'title', 'moral']));
+                });
             });
         });
     });
